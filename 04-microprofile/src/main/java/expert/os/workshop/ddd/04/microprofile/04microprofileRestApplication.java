bb02f350ce9b638d007a6429cdfa53d9ce2f54c7
Otavio Santana <otaviopolianasantana@gmail.com>
2023-02-17 21:36:04 +0000

    add new project
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ 04-microprofile/src/main/java/expert/os/workshop/ddd/04/microprofile/04microprofileRestApplication.java
@@ -0,0 +1,11 @@
+package expert.os.workshop.ddd.04.microprofile;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class 04microprofileRestApplication extends Application {
+}
