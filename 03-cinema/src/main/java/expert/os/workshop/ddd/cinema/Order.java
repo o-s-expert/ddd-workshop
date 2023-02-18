e524399542c0be8cb8853e6eb2f513a8c64d4d2c
Otavio Santana <otaviopolianasantana@gmail.com>
2023-02-18 06:02:04 +0000

    Update order
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 03-cinema/src/main/java/expert/os/workshop/ddd/cinema/Order.java
+++ 03-cinema/src/main/java/expert/os/workshop/ddd/cinema/Order.java
@@ -1,11 +1,44 @@
 package expert.os.workshop.ddd.cinema;
 
-import java.util.ArrayList;
-import java.util.List;
+import org.jmolecules.ddd.annotation.Entity;
+import org.jmolecules.ddd.annotation.Identity;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+@Entity
 public class Order {
 
+    @Identity
+    private String id;
     private Moviegoer moviegoer;
 
     private List<Product> products = new ArrayList<>();
+
+    public Moviegoer getMoviegoer() {
+        return moviegoer;
+    }
+
+    public List<Product> getProducts() {
+        return Collections.unmodifiableList(products);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Order order = (Order) o;
+        return Objects.equals(moviegoer, order.moviegoer);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(moviegoer);
+    }
 }
