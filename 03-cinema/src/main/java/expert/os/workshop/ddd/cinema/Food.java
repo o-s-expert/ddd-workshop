1281a698cc00a42f5689838bfc7d771aa106c592
Otavio Santana <otaviopolianasantana@gmail.com>
2023-02-18 06:12:50 +0000

    Update food structure
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- 03-cinema/src/main/java/expert/os/workshop/ddd/cinema/Food.java
+++ 03-cinema/src/main/java/expert/os/workshop/ddd/cinema/Food.java
@@ -1,15 +1,45 @@
 package expert.os.workshop.ddd.cinema;
 
 import javax.money.MonetaryAmount;
+import java.util.Objects;
 
-public class Food implements Product{
+public class Food implements Product {
 
     private String name;
 
     private MonetaryAmount price;
 
+    public String getName() {
+        return name;
+    }
+
     @Override
     public MonetaryAmount price() {
         return price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Food food = (Food) o;
+        return Objects.equals(name, food.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
+    }
+
+    @Override
+    public String toString() {
+        return "Food{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
     }
 }
