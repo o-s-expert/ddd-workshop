98eca51c588c7deaa78d3ca7f9a696c8ce2ec5e4
Otavio Santana <otaviopolianasantana@gmail.com>
2023-03-13 19:44:34 +0000

    chore: add microprofile open-liberty code
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ 08-microstream-microservices/src/main/java/expert/os/workshop/ddd/microstream/microservices/MicrostreammicroservicesRestApplication.java
@@ -0,0 +1,11 @@
+package expert.os.workshop.ddd.microstream.microservices;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ *
+ */
+@ApplicationPath("/data")
+public class MicrostreammicroservicesRestApplication extends Application {
+}
