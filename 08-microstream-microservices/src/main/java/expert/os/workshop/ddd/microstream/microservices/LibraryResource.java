09880829a36d99f8f28430bc3db8e2bbc1be5e8c
Otavio Santana <otaviopolianasantana@gmail.com>
2023-03-13 20:00:35 +0000

    feat: create library resource
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

---
+++ 08-microstream-microservices/src/main/java/expert/os/workshop/ddd/microstream/microservices/LibraryResource.java
@@ -0,0 +1,54 @@
+package expert.os.workshop.ddd.microstream.microservices;
+
+
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import java.util.List;
+import java.util.Optional;
+
+import static jakarta.ws.rs.core.Response.Status.NOT_FOUND;
+
+@Path("/hello")
+@RequestScoped
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class LibraryResource {
+
+    @Inject
+    private Library library;
+
+
+    @GET
+    public List<Book> findAll() {
+        return this.findAll();
+    }
+
+    @GET
+    @Path("{id}")
+    public Book findById(@PathParam("id") String id) {
+        Optional<Book> book = this.library.findById(id);
+        return book.orElseThrow(() -> new WebApplicationException(NOT_FOUND));
+    }
+
+    @PUT
+    public void save(Book book) {
+
+    }
+
+    @DELETE
+    @Path("{id}")
+    public void delete(@PathParam("id") String id) {
+        this.delete(id);
+    }
+}
