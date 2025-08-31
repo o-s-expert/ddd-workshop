c33be801e9381af6fc466a1e6fc3a18665c95783
Otavio Santana <otaviopolianasantana@gmail.com>
2025-08-31 20:26:50 +0100

    test: add architecture test to enforce domain layer independence

---
+++ solutions/03-ecommerce/src/test/java/com/example/ecommerce/JMoleculesDddUnitTest.java
@@ -0,0 +1,26 @@
+package com.example.ecommerce;
+
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import org.jmolecules.archunit.JMoleculesArchitectureRules;
+import org.jmolecules.archunit.JMoleculesDddRules;
+import org.junit.jupiter.api.Test;
+
+public class JMoleculesDddUnitTest {
+
+    @Test
+    void checkTheLayerIntegration() {
+        String packageName = SimpleGreetResource.class.getPackageName();
+        JavaClasses classes = new ClassFileImporter().importPackages(packageName);
+        JMoleculesArchitectureRules.ensureLayering().check(classes);
+
+    }
+
+    @Test
+    void checkDDDIntegration() {
+        String packageName = SimpleGreetResource.class.getPackageName();
+        JavaClasses classes = new ClassFileImporter().importPackages(packageName);
+        JMoleculesDddRules.all().check(classes);
+    }
+}
