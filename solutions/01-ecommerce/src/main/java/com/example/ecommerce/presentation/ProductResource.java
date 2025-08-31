f6be6b3f21972b6eb07286b1e4866c1308215667
Otavio Santana <otaviopolianasantana@gmail.com>
2025-08-31 17:38:24 +0100

    feat: add ProductResource for product API endpoints

--- solutions/01-ecommerce/src/main/java/com/example/ecommerce/presentation/ProductResource.java
+++ solutions/01-ecommerce/src/main/java/com/example/ecommerce/presentation/ProductResource.java
@@ -1,6 +1,59 @@
 package com.example.ecommerce.presentation;
 
+
+import com.example.ecommerce.domain.Product;
+import com.example.ecommerce.domain.ProductService;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.WebApplicationException;
+import jakarta.ws.rs.core.Response;
+
+import java.util.List;
+
+@ApplicationScoped
+@Path("/products")
 public class ProductResource {
 
+    private final ProductMapper mapper;
+
+    private final ProductService productService;
+
+    @Inject
+    public ProductResource(ProductMapper mapper, ProductService productService) {
+        this.mapper = mapper;
+        this.productService = productService;
+    }
+
+
+    @GET
+    public List<ProductResponse> getAllProducts() {
+        return productService.findAll().stream()
+                .map(mapper::toResponse)
+                .toList();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Product getProductById(@PathParam("id") String id) {
+        return productService.findById(id).orElseThrow(
+                () -> new WebApplicationException("Product with id " + id + " not found",
+                        Response.Status.NOT_FOUND));
+    }
+
+
+    @GET
+    @Path("/{id}")
+    public void deleteProductById(@PathParam("id") String id) {
+        productService.delete(id);
+    }
+
+    @POST
+    public void insert(ProductRequest request) {
+        productService.save(mapper.toDomain(request));
+    }
 
 }
