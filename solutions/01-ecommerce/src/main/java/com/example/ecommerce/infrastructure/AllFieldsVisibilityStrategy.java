3f7566de4bc07f054c85e7857497be508acf35af
Otavio Santana <otaviopolianasantana@gmail.com>
2025-08-31 16:46:37 +0100

    feat: add AllFieldsVisibilityStrategy for property visibility configuration

---
+++ solutions/01-ecommerce/src/main/java/com/example/ecommerce/infrastructure/AllFieldsVisibilityStrategy.java
@@ -0,0 +1,18 @@
+package com.example.ecommerce.infrastructure;
+
+import jakarta.json.bind.config.PropertyVisibilityStrategy;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class AllFieldsVisibilityStrategy implements PropertyVisibilityStrategy {
+    @Override
+    public boolean isVisible(Field field) {
+        return true;
+    }
+
+    @Override
+    public boolean isVisible(Method method) {
+        return true;
+    }
+}
