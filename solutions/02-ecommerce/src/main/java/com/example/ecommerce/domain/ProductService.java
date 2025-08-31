1ca581f844f22ae285426fddb4fcd39f241255bb
Otavio Santana <otaviopolianasantana@gmail.com>
2025-08-31 19:45:42 +0100

    feat: add changePrice method to ProductService for updating product prices

--- solutions/02-ecommerce/src/main/java/com/example/ecommerce/domain/ProductService.java
+++ solutions/02-ecommerce/src/main/java/com/example/ecommerce/domain/ProductService.java
@@ -3,6 +3,7 @@ package com.example.ecommerce.domain;
 import com.example.ecommerce.annotations.ApplicationService;
 import jakarta.inject.Inject;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,9 +12,12 @@ public class ProductService {
 
     private final ProductRepository productRepository;
 
+    private final UpdateProductPrice updateProductPrice;
+
     @Inject
-    public ProductService(ProductRepository productRepository) {
+    public ProductService(ProductRepository productRepository, UpdateProductPrice updateProductPrice) {
         this.productRepository = productRepository;
+        this.updateProductPrice = updateProductPrice;
     }
 
     public Product save(Product product) {
@@ -30,5 +34,12 @@ public class ProductService {
 
     public List<Product> findAll() {
         return productRepository.findAll();
+    }
+
+    public void changePrice(String id, BigDecimal price) {
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Product with id " + id + " not found"));
+        //we could improve this exception as well.
+        this.updateProductPrice.execute(product, price);
     }
 }
