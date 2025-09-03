eeac1db24a963de2075efcb6f4369c555246e22c
Otavio Santana <otaviopolianasantana@gmail.com>
2025-09-03 05:25:41 +0200

    refactor: rename productRepository to repository for consistency

--- solutions/01-ecommerce/src/main/java/com/example/ecommerce/domain/ProductService.java
+++ solutions/01-ecommerce/src/main/java/com/example/ecommerce/domain/ProductService.java
@@ -9,26 +9,26 @@ import java.util.Optional;
 @ApplicationService
 public class ProductService {
 
-    private final ProductRepository productRepository;
+    private final ProductRepository repository;
 
     @Inject
-    public ProductService(ProductRepository productRepository) {
-        this.productRepository = productRepository;
+    public ProductService(ProductRepository repository) {
+        this.repository = repository;
     }
 
     public Product save(Product product) {
-        return productRepository.save(product);
+        return repository.save(product);
     }
 
     public void deleteById(String id) {
-        productRepository.deleteById(id);
+        repository.deleteById(id);
     }
 
     public Optional<Product> findById(String id) {
-        return productRepository.findById(id);
+        return repository.findById(id);
     }
 
     public List<Product> findAll() {
-        return productRepository.findAll();
+        return repository.findAll();
     }
 }
